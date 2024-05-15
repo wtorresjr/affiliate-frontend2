23c9555df7f37e4e3daeaa518783d7604ab7fdd8
Will Torres <w.torresjr@gmail.com>
2024-05-15 17:19:22 -0400

    Properly styled header for small screens

--- src/components/Header1/index.jsx
+++ src/components/Header1/index.jsx
@@ -12,52 +12,52 @@ export default function Header1({ ...props }) {
   const pathname = usePathname();
   return (
     <header {...props} className={`${props.className}`}>
-      {/* <div className="flex gap-[22px] w-full"> */}
-      <Input
-        name="Search Field"
-        placeholder={`Search`}
-        value={searchBarValue}
-        onChange={(e) => setSearchBarValue(e)}
-        suffix={
-          <div className="flex items-center justify-center">
-            {searchBarValue?.length > 0 ? (
-              <CloseSVG
-                onClick={() => setSearchBarValue("")}
-                height={29}
-                width={29}
-              />
-            ) : (
-              <Img
-                src="img_rewind.svg"
-                width={29}
-                height={29}
-                alt="rewind"
-                className="h-[29px] w-[29px] cursor-pointer"
-              />
-            )}
-          </div>
-        }
-        className="flex !h-[51px] flex-grow rounded-lg items-center justify-end gap-[35px] bg-white-A700 text-lg text-blue_gray-100"
-      />
+      <div className="flex gap-[22px] w-full">
+        <Input
+          name="Search Field"
+          placeholder={`Search`}
+          value={searchBarValue}
+          onChange={(e) => setSearchBarValue(e)}
+          suffix={
+            <div className="flex items-center justify-center">
+              {searchBarValue?.length > 0 ? (
+                <CloseSVG
+                  onClick={() => setSearchBarValue("")}
+                  height={29}
+                  width={29}
+                />
+              ) : (
+                <Img
+                  src="img_rewind.svg"
+                  width={29}
+                  height={29}
+                  alt="rewind"
+                  className="h-[29px] w-[29px] cursor-pointer"
+                />
+              )}
+            </div>
+          }
+          className="flex !h-[51px] flex-grow rounded-lg items-center justify-end gap-[35px] bg-white-A700 text-lg text-blue_gray-100"
+        />
 
-      <Button
-        size="lg"
-        shape="round"
-        leftIcon={
-          <Img
-            src="img_plus_white_a700.svg"
-            width={23}
-            height={23}
-            alt="plus"
-            className="h-[23px] w-[23px]"
-          />
-        }
-        className="flex gap-[15px] bg-[#FF3B2E] text-white-A700  rounded-[6px] sm:w-1/4"
-      >
-        <span className="sm:hidden">Add New Affiliate</span>
-      </Button>
-      {/* </div> */}
-      <div className="flex justify-end items-center gap-[19px] md:w-full">
+        <Button
+          size="lg"
+          shape="round"
+          leftIcon={
+            <Img
+              src="img_plus_white_a700.svg"
+              width={23}
+              height={23}
+              alt="plus"
+              className="h-[23px] w-[23px]"
+            />
+          }
+          className="flex gap-[15px] bg-[#FF3B2E] text-white-A700  rounded-[6px] sm:w-1/4"
+        >
+          <span className="sm:hidden">Add New Affiliate</span>
+        </Button>
+      </div>
+      <div className="sm:flex justify-between items-center">
         <Button shape="round" className="w-[51px] shadow-sm bg-white-A700">
           <Img src="img_vector.svg" width={51} height={51} />
         </Button>
