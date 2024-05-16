8f6598e44c5dc209ce5ac03e033db15d1974b7ae
Will Torres <w.torresjr@gmail.com>
2024-05-16 01:24:33 -0400

    Refactored Upload Download components to be more DRY

--- src/components/DownloadComponent/index.jsx
+++ src/components/DownloadComponent/index.jsx
@@ -1,24 +1,26 @@
-import { Heading, Img, Text } from "../../app/components-main"
-const DownloadComponent = ({itemTitle}) => {
-    return (
-        <>
-            <div className="flex flex-col gap-[23px]">
-            <div className="flex h-[147px] w-[178px] items-center justify-center aspect-w-1 aspect-h-1 bg-[url(/images/img_group_555.svg)] cursor-pointer hover:bg-[url(/images/img_group_554.svg)] bg-cover bg-no-repeat px-[46px] pb-[26px] pt-[46px]">
-              <Img
-                src="img_user.svg"
-                width={49}
-                height={73}
-                alt="userimage"
-                className="h-[73px] w-[49]"
-              />
-            </div>
-          <Text size="lg" as="p" className="pb-6 ml-3.5 md:ml-0">
-              {itemTitle}
-          </Text>
-          
-          </div>
-        </>
-    )
-}
+import { Heading, Img, Text } from "../../app/components-main";
 
-export {DownloadComponent}
+const DownloadComponent = ({ itemTitle }) => {
+  return (
+    <>
+      <div className="flex flex-col gap-[23px]">
+        <div className="flex h-[147px] w-[178px] items-center justify-center aspect-w-1 aspect-h-1 bg-[url(/images/img_group_555.svg)] cursor-pointer hover:bg-[url(/images/img_group_554.svg)] bg-cover bg-no-repeat">
+          <Img
+            src="img_user.svg"
+            width={49}
+            height={73}
+            alt="userimage"
+            className="h-[73px] w-[49]"
+          />
+        </div>
+        <Text size="lg" as="p" className="pb-6 ml-3.5 md:ml-0">
+          {itemTitle}
+        </Text>
+      </div>
+    </>
+  );
+};
+
+export { DownloadComponent };
+
+
--- src/components/UploadComponent/index.jsx
+++ src/components/UploadComponent/index.jsx
@@ -1,51 +1,50 @@
-import { Heading, Img, Text } from "../../app/components-main"
-const UploadComponent = ({itemTitle}) => {
-    return (
-        <>
-            
-            <div className="flex flex-col pt-6 gap-[23px]">
-            <Text size="lg" as="p" className="ml-3.5 md:ml-0">
-              {itemTitle}
-            </Text>
-            <div className="flex h-[147px] w-[178px] items-center justify-center aspect-w-1 aspect-h-1 bg-[url(/images/img_group_555.svg)] cursor-pointer hover:bg-[url(/images/img_group_554.svg)] bg-cover bg-no-repeat px-[46px] pb-[26px] pt-[46px]">
-              <Img
-                src="img_plus.svg"
-                width={46}
-                height={46}
-                alt="plus"
-                className="h-[46px] w-[46]"
-              />
-                </div>
-                
-            {itemTitle === "Brand Images" &&
-            <div className="ml-4 flex flex-col items-start gap-[3px] md:ml-0">
-              <Text size="s" as="p" className="!text-[9.35px] !text-gray-500">
-                Examples:
-              </Text>
-              <Text size="s" as="p" className="!text-[9.35px] !text-gray-500">
-                Logo Files
-              </Text>
-              <Text size="s" as="p" className="!text-[9.35px] !text-gray-500">
-                Branding Graphics
-              </Text>
-              <Text size="s" as="p" className="!text-[9.35px] !text-gray-500">
-                Color Swatches
-              </Text>
-            </div>
-                }
-            {itemTitle === "Brand Documents" && 
-                <div className="ml-4 flex flex-col items-start gap-[3px] md:ml-0">
-            <Text size="s" as="p" className="!text-[9.35px] !text-gray-500">
-              Examples:
-            </Text>
-            <Text size="s" as="p" className="!text-[9.35px] !text-gray-500">
-              Brand Guide PDF or Word Doc
-            </Text>
-          </div> 
-                }
-          </div>
-        </>
-    )
-}
+import { Heading, Img, Text } from "../../app/components-main";
+const UploadComponent = ({ itemTitle }) => {
+  return (
+    <>
+      <div className="flex flex-col pt-6 gap-[23px]">
+        <Text size="lg" as="p" className="ml-3.5 md:ml-0">
+          {itemTitle}
+        </Text>
+        <div className="flex h-[147px] w-[178px] items-center justify-center aspect-w-1 aspect-h-1 bg-[url(/images/img_group_555.svg)] cursor-pointer hover:bg-[url(/images/img_group_554.svg)] bg-cover bg-no-repeat px-[46px] pb-[26px] pt-[46px]">
+          <Img
+            src="img_plus.svg"
+            width={46}
+            height={46}
+            alt="plus"
+            className="h-[46px] w-[46]"
+          />
+        </div>
 
-export {UploadComponent}
+        {itemTitle === "Brand Images" && (
+          <div className="ml-4 flex flex-col gap-[3px] md:ml-0">
+            <Text
+              size="s"
+              as="p"
+              className="!text-[9.35px] !text-gray-500 space-y-[3px]"
+            >
+              <p>Examples:</p>
+              <p>Logo Files</p>
+              <p>Branding Graphics</p>
+              <p>Color Swatches</p>
+            </Text>
+          </div>
+        )}
+        {itemTitle === "Brand Documents" && (
+          <div className="ml-4 flex flex-col gap-[3px] md:ml-0">
+            <Text
+              size="s"
+              as="p"
+              className="!text-[9.35px] !text-gray-500 space-y-[3px]"
+            >
+              <p>Examples:</p>
+              <p>Brand Guide PDF or Word Doc</p>
+            </Text>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export { UploadComponent };
