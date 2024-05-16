38e04492176de7062806924dff213df16a04fe7c
Will Torres <w.torresjr@gmail.com>
2024-05-16 19:03:28 -0400

    tweaked the look of campaign buttons

--- src/app/components-main/WireframeCampaigns1OneUserprofile/index.jsx
+++ src/app/components-main/WireframeCampaigns1OneUserprofile/index.jsx
@@ -9,7 +9,6 @@ export default function WireframeCampaigns1OneUserprofile({
   iconBgColor,
   // ...props
 }) {
-  console.log(iconBgColor);
   return (
     // <div
     //   {...props}
@@ -25,15 +24,16 @@ export default function WireframeCampaigns1OneUserprofile({
           <Img src={image} width={45} height={45} />
         </Button>
       </div>
-      <div className="sm:pl-2 w-1/2 align-top">
-        <Text size="s" as="p" className="sm:w-full !text-[14px] !text-gray-500">
-          {commission}
-        </Text>
+
+      <div className="sm:pl-2 pr-2 w-1/2 align-top">
+      <Text size="s" as="p" className="sm:w-full !text-[14px] !text-gray-500 text-right">
+      {commission}
+      </Text>
       </div>
       <div className="sm:flex items-center right-8">
-        <Text size="xxl" as="p" className="sm:!text-lg pr-2">
-          {amount}
-        </Text>
+      <Text size="xxl" as="p" className="sm:!text-lg pr-2">
+      {amount}
+      </Text>
         <Img
           src={"img_vector_5x3.svg"}
           width={3}
@@ -41,11 +41,29 @@ export default function WireframeCampaigns1OneUserprofile({
           alt="image"
           className="h-[5px] self-start"
         />
-        <Text size="s" as="p" className="!text-xs !text-red-500_01">
-          <span className="text-red-500_01">{`${percentage}%`}</span>
-          <span className="text-black-900">&nbsp;this month</span>
-        </Text>
-      </div>
+        <Text size="s" as="p" className="sm:!text-xs !text-red-500_01 text-right">
+        <span className="text-red-500_01">{`${percentage}%`}</span>
+        <span className="text-black-900">&nbsp;this month</span>
+      </Text>
+      </div> 
     </div>
   );
 }
+
+
+      // <div className="flex flex-row">
+      //   {commission}
+      //   {amount}
+      // </div>
+      // <div className="flex flex-row w-full">
+      //   <Img
+      //     src={"img_vector_5x3.svg"}
+      //     width={3}
+      //     height={5}
+      //     alt="image"
+      //     className="h-[5px] self-start"
+      //   />
+      //   {/* <Text size="s" as="p" className="!text-xs !text-red-500_01"> */}
+      //   <span className="text-red-500_01">{`${percentage}%`}</span>
+      //   <span className="text-black-900">&nbsp;this month</span>
+      // </div>
