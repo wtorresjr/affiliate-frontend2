1de878416f3e13c9032a0e838772283550871c71
Will Torres <w.torresjr@gmail.com>
2024-05-16 22:59:02 -0400

    Refactored metrics view for small screens

--- src/app/campaigns/index-n.jsx
+++ src/app/campaigns/index-n.jsx
@@ -1,27 +1,69 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { campaignData } from "../helperSeedData";
-import { Button, DateRange, Img, Text } from "../../components";
+import { Button, DateRange, Img, Text, SelectBox } from "../../components";
 import WireframeCampaigns1OneUserprofile from "../components-main/WireframeCampaigns1OneUserprofile";
 import Header1 from "../../components/Header1";
 import "../../styles/tailwind.css";
 
 export const CampaignPageView = () => {
+  const [selected, setSelected] = useState(null);
+  const [selectedDetails, setSelectedDetails] = useState(null);
+
+  useEffect(() => {
+    const chosenMetric = campaignData.find(
+      (metric) => metric.label === selected,
+    );
+    setSelectedDetails(chosenMetric);
+  }, [selected, selectedDetails]);
+
   return (
     <>
       <Header1 className="sm:flex flex-col-reverse gap-3 pb-3" />
       <DateRange />
 
-      <div className="sm:flex flex-col flex-wrap gap-[5px] mt-3">
-        {campaignData.map((d, index) => (
-          <WireframeCampaigns1OneUserprofile
-            {...d}
-            key={"userprofile" + index}
-            className="px-2.5 pt-2.5"
-            campData={d}
-          />
+      <select
+        className="bg-white-A700 mt-3 p-4 rounded-[8px] appearance-none flex flex-row justify-between"
+        onChange={(e) => setSelected(e.target.value)}
+      >
+        <option>Select a Metric</option>
+        {campaignData.map((option) => (
+          <option
+            key={option.value}
+            value={option.label}
+            className="border-black-900 border-solid border-x-2 flex flex-row justify-between w-full"
+          >
+            {option.label}
+          </option>
         ))}
-      </div>
+      </select>
+      {selectedDetails ? (
+        <div
+          className="sm:flex flex-row justify-between items-center rounded-[8px] p-2 bg-green-200 mt-3"
+          key={selectedDetails.image}
+        >
+          <Img src={selectedDetails.image} width={25} height={25} />
+          <div>{selectedDetails.value}</div>
+
+          <div className="sm:flex justify-between items-center gap-3">
+            <Img src="img_vector_2.svg" width={10} height={10} />
+            <span className="text-red-500_01">{`${selectedDetails.percentage}%`}</span>
+            <span className="text-black-900">&nbsp;this month</span>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };
+
+// <div className="sm:flex flex-wrap gap-[5px] mt-3">
+//   {campaignData.map((d, index) => (
+//     <WireframeCampaigns1OneUserprofile
+//       {...d}
+//       key={"userprofile" + index}
+//       className="px-2.5 pt-2.5"
+//       campData={d}
+//     />
+//   ))}
+// </div>
--- src/app/components-main/WireframeCampaigns1OneUserprofile/index.jsx
+++ src/app/components-main/WireframeCampaigns1OneUserprofile/index.jsx
@@ -4,18 +4,45 @@ import { Text, Img, Button } from "./..";
 export default function WireframeCampaigns1OneUserprofile({
   image,
   percentage,
-  commission,
-  amount,
+  label,
+  value,
   iconBgColor,
   // ...props
 }) {
-  return (
-    // <div
-    //   {...props}
-    //   className={`${props.className} sm:flex items-center pb-3 border-black-900 border-[0.5px] border-solid bg-indigo-50 rounded-[20px] active:bg-white-A700`}
-    // >
+  return <></>;
+}
+
+//  <div className="sm:flex flex-row flex-wrap w-1/3 flex-grow box-border p-1 border-black-900 border border-solid rounded-[8px]">
+//       <div className="sm:flex items-center gap-2">
+//         <Button
+//           size="md"
+//           className="w-[40px] h-[40px] rounded-[22px]"
+//           style={{ backgroundColor: iconBgColor }}
+//         >
+//           <Img src={image} width={25} height={25} />
+//         </Button>
+//         <Text>{label}</Text>
+//       </div>
+//       <div className="sm:flex w-full items-center justify-end">
+//         <Img
+//           src={"img_vector_2.svg"}
+//           width={3}
+//           height={5}
+//           alt="image"
+//           className="h-[12px] w-[12px]"
+//         />
+//         <Text>
+//           <span className="text-red-500_01">{`${percentage}%`}</span>
+//           <span className="text-black-900">&nbsp;this month</span>
+//         </Text>
+//       </div>
+//     </div>
+
+{
+  /*
     <div className="sm:flex flex-row items-center pb-1 pt-1 border-black-900 border-[0.5px] border-solid bg-indigo-50 rounded-[20px] active:bg-white-A700 pl-2 pr-2">
-      <div className="sm:flex flex-row">
+    </div>
+ <div className="sm:flex flex-row">
         <Button
           size="md"
           className="w-[40px] h-[40px] rounded-[22px]"
@@ -25,45 +52,29 @@ export default function WireframeCampaigns1OneUserprofile({
         </Button>
       </div>
 
-      <div className="sm:pl-2 pr-2 w-1/2 align-top">
-      <Text size="s" as="p" className="sm:w-full !text-[14px] !text-gray-500 text-right">
-      {commission}
-      </Text>
+      <div className="sm:pl-2 pr-3 w-1/2 align-top text-left">
+        <Text size="s" as="p" className="sm:w-full !text-[14px] !text-gray-500">
+          {label}
+        </Text>
       </div>
       <div className="sm:flex items-center right-8">
-      <Text size="xxl" as="p" className="sm:!text-lg pr-2">
-      {amount}
-      </Text>
+        <Text size="xxl" as="p" className="sm:!text-lg pr-2 pl-2">
+          {value}
+        </Text>
         <Img
-          src={"img_vector_5x3.svg"}
+          src={"img_vector_2.svg"}
           width={3}
           height={5}
           alt="image"
-          className="h-[5px] self-start"
+          className="h-[20px] w-[20px]"
         />
-        <Text size="s" as="p" className="sm:!text-xs !text-red-500_01 text-right">
-        <span className="text-red-500_01">{`${percentage}%`}</span>
-        <span className="text-black-900">&nbsp;this month</span>
-      </Text>
-      </div> 
-    </div>
-  );
+        <Text
+          size="s"
+          as="p"
+          className="sm:!text-xs !text-red-500_01 text-right"
+        >
+          <span className="text-red-500_01">{`${percentage}%`}</span>
+          <span className="text-black-900">&nbsp;this month</span>
+        </Text>
+      </div> */
 }
-
-
-      // <div className="flex flex-row">
-      //   {commission}
-      //   {amount}
-      // </div>
-      // <div className="flex flex-row w-full">
-      //   <Img
-      //     src={"img_vector_5x3.svg"}
-      //     width={3}
-      //     height={5}
-      //     alt="image"
-      //     className="h-[5px] self-start"
-      //   />
-      //   {/* <Text size="s" as="p" className="!text-xs !text-red-500_01"> */}
-      //   <span className="text-red-500_01">{`${percentage}%`}</span>
-      //   <span className="text-black-900">&nbsp;this month</span>
-      // </div>
